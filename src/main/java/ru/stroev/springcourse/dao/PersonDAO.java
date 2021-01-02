4770081a8830fbdfda00d5e6e3261c2d9d856972
Vladlen Stroev <totlil@bk.ru>
2021-01-02 10:43:57 +0300

    Урок 22

---
+++ src/main/java/ru/stroev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,34 @@
+package ru.stroev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.stroev.springcourse.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT,"Masha"));
+        people.add(new Person(++PEOPLE_COUNT,"Vasy"));
+        people.add(new Person(++PEOPLE_COUNT,"Anna"));
+        people.add(new Person(++PEOPLE_COUNT,"Vlad"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+}
